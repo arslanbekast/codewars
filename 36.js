0d00035c0b65a0077e1484883d36cc0a9bbb0cf7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-01-14 18:25:39 +0300

    Another task solved

---
+++ 36.js
@@ -0,0 +1,6 @@
+// Each number should be formatted that it is rounded to two decimal places.
+// You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+const twoDecimalPlaces = n => +n.toFixed(2)
+
+console.log(twoDecimalPlaces(4.653725356))
