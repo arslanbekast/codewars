e920d7181f6468ff3eb712def96af094d86fb5ba
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-01-03 17:31:05 +0300

    Another task solved

---
+++ 60.js
@@ -0,0 +1,6 @@
+// Implement a function that adds two numbers together and returns their sum in binary.
+// The conversion can be done before, or after the addition.
+
+const addBinary = (a,b) => (a + b).toString(2)
+
+console.log(addBinary(1,1))
