76be92e90ff61ac1cc624bd17ed45964eb9ec37d
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-01-16 15:56:30 +0300

    Another task solved

--- 79.js
+++ 79.js
@@ -1,6 +1,16 @@
-// Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-// The first word within the output should be capitalized only if the original word was capitalized
-// (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+//
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+// Requirements:
+//
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+// Division should be integer division. For example, this should return 2, not 2.666666...:
 
 function zero(op) { return op ? op(0) : 0; }
 function one(op) { return op ? op(1) : 1; }
