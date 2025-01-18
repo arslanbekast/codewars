012a33e13ce117dd25cb585ce37ef19382a29a17
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-01-18 14:29:10 +0300

    Another task solved

---
+++ 82.js
@@ -0,0 +1,19 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+    let result = {}
+    const uniqChars = [...new Set(string)]
+    uniqChars.forEach(char => {
+        result = {
+            ...result,
+            [char]: string.split('').filter(el => el === char).length
+        }
+    })
+    return result
+}
+
+console.log(count('aba'))
+
+
