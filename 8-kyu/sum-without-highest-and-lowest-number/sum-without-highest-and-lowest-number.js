2176ff148549f7084d05b3d199752ae1e65ac60c
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-11-14 18:52:50 +0300

    Changes

--- 8-kyu/sum-without-highest-and-lowest-number/sum-without-highest-and-lowest-number.js
+++ 8-kyu/sum-without-highest-and-lowest-number/sum-without-highest-and-lowest-number.js
@@ -2,6 +2,6 @@ const sumArray = (array) => {
     if (!Array.isArray(array) || array.length <= 1) {
         return 0;
     }
-​
+
     return array.sort((a, b) => a - b).slice(1, array.length - 1).reduce((a, b) => a + b, 0);
 }
